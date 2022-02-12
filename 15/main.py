bf5a98ebed792ecfd5e668e87582a4f1bf1cf1ee
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2022-02-12 23:48:28 +0300

    rewrite coffe machine in OOP style

--- 15/main.py
+++ 15/main.py
@@ -1,9 +1,23 @@
-"""rifme generator"""
-import requests
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-r = requests.get('https://rifmus.net/rifma/%D0%B4%D0%B2%D0%B0%D0%B4%D1%86%D0%B0%D1%82%D1%8C', auth=('user', 'pass'))
-print(r.status_code)
-print(r.headers['content-type'])
-print(r.encoding)
-print(r.text)
-print(r.json())
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+          coffee_maker.make_coffee(drink)
