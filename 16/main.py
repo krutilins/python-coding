66e1255e94f383c3fac7178f8e3be81db66ebdfc
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2022-02-13 23:36:34 +0300

    update quiz

--- 16/main.py
+++ 16/main.py
@@ -1,23 +1,26 @@
-from typing import List
-
-def chooseBetweenOptions(question: str, options: List[str]):
-  comma_separeted_options = ', '.join(options) 
-  while True:
-    chosen_option = input(f"{question} ({comma_separeted_options}): ")
-    if options.count(chosen_option) > 0:
-      return(chosen_option)
-    else:
-      print("Unknown operation. Try again!")
-
-
 class Question:
-  def __init__(self, question, answer):
+  def tryAnswer(self):
+    return(self.answer is self.chooseBetweenOptions())
+
+  def chooseBetweenOptions(self):
+    print(self.options)
+    comma_separeted_options = ', '.join(self.options) 
+    while True:
+      chosen_option = input(f"{self.question} ({comma_separeted_options}): ")
+      if self.options.count(chosen_option) > 0:
+        return(chosen_option)
+      else:
+        print("Unknown operation. Try again!")
+
+  def __init__(self, question, answer, incorrect_options):
     self.question = question
     self.answer = answer
+    incorrect_options.append(answer)
+    self.options = incorrect_options
 
-q1= Question("2 + 3 = 5", "True")
+q1= Question("2 + 3 = 5", "True", ["False"])
 
-if q1.answer is chooseBetweenOptions(q1.question, ["True", "False"]):
+if q1.tryAnswer():
   print("You are right!")
 else:
   print("You are wrong!") 
